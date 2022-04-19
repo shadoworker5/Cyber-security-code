04c67946d47844cb912c9ae4181c502503c2391f
Kassoum <fofdhy007@yahoo.fr>
2022-04-19 14:20:14 +0000

    Delete unuse code

--- camera.py
+++ camera.py
@@ -2,8 +2,6 @@
  * @author Kassoum TRAORE
  * @email shadoworker5.dev@gmail.com
  * @create date 2022-02-14 23:06:22
- * @modify date 2022-04-18 23:07:02
+ * @modify date 2022-04-19 14:19:10
  * @desc [description]
 """
-
-
