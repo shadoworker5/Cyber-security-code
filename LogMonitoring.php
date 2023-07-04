83f6252528629078dcf831c97471b22a568bd1ac
Kassoum Traore <kassoum.traore@africasys.com>
2023-07-04 11:30:53 +0000

    update ip_track and LogMonitoring

--- LogMonitoring.php
+++ LogMonitoring.php
@@ -2,34 +2,34 @@
 /*
 * @author Shadoworker5 Dev
 * @email shadoworker5.dev@gmail.com
-* @create date 2023-01-11 ‏‎23:05:29
-* @modify date 2023-01-12 17:17:51
+* @create date 2023-01-11 23:05:29
+* @modify date 2023-07-04 10:47:06
 * @desc [description]
 */
 
 class LogMonitoring {
-    private $support_address        = 'example@example.xyz';
-    private $mail_subject           = 'An Exception occur in your application';
-    private $slack_bot_url          = 'https://hooks.slack.com/services/XXXXXXXXX/XXXXXXXXX/XXXXXXXXXXXXXXXXXX';
-    private $discord_webhooks_url   = 'https://discordapp.com/api/webhooks/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+    private static $support_address        = 'example@example.xyz';
+    private static $mail_subject           = 'An Exception occur in your application';
+    private static $slack_bot_url          = 'https://hooks.slack.com/services/XXXXXXXXX/XXXXXXXXX/XXXXXXXXXXXXXXXXXX';
+    private static $discord_webhooks_url   = 'https://discordapp.com/api/webhooks/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
 
-    public function sendByMail($content) {
+    public static function sendByMail($content) {
         try {
             $message = 'Hi. Something went wrong in your application. Some detail here: '. json_encode($content);
-            mail($this->support_address, $this->mail_subject, $message);
+            mail(self::$support_address, self::$mail_subject, $message);
         } catch (Exception $e) {
-            $this->sendBySlack($content);
-            $this->sendBySlack('Error in your Mail user. Detail: '.json_encode($e));
+            self::sendBySlack($content);
+            self::sendBySlack('Error sendByMail: '.json_encode($e));
         }
     }
 
-    public function sendBySlack($content) {
+    public static function sendBySlack($content) {
         try {
             $query_init_curl    = curl_init();
             $header             = ['Content-Type: application'];
             $query_data         = "{'text': '$content'}";
     
-            curl_setopt($query_init_curl, CURLOPT_URL,              $this->slack_bot_url);
+            curl_setopt($query_init_curl, CURLOPT_URL,              self::$slack_bot_url);
             curl_setopt($query_init_curl, CURLOPT_RETURNTRANSFER,   true);
             curl_setopt($query_init_curl, CURLOPT_POST,             1);
             curl_setopt($query_init_curl, CURLOPT_POSTFIELDS,       $query_data);
@@ -38,14 +38,14 @@ class LogMonitoring {
             curl_exec($query_init_curl);
             curl_close($query_init_curl);
         } catch (Exception $e) {
-            $this->sendByDiscord($content);
-            $this->sendByDiscord('Error in your Slack bot. Detail: '.json_encode($e));
+            self::sendByDiscord($content);
+            self::sendByDiscord('Error in your Slack bot. Detail: '.json_encode($e));
         }
     }
  
-    public function sendByDiscord($content) {
+    public static function sendByDiscord($content) {
         try {
-            $url                = $this->discord_webhooks_url;
+            $url                = self::$discord_webhooks_url;
             $query_init_curl    = curl_init();
             $header             = ['Content-Type: application/json'];
             $message            = 'Hi. Something went wrong in your application. Some detail here: '. json_encode($content);
@@ -61,13 +61,11 @@ class LogMonitoring {
             curl_exec($query_init_curl);
             curl_close($query_init_curl);
         } catch (Exception $e) {
-            $this->sendBySlack($content);
-            $this->sendBySlack('Error in your Discord bot. Detail: '.json_encode($e));
+            self::sendBySlack($content);
+            self::sendBySlack('Error in your Discord bot. Detail: '.json_encode($e));
         }
     }
 }
 
-$message = "Hello world form log monitor";
-$monitor = new LogMonitoring();
-$monitor->sendByMail($message);
-echo $message;
+$message = 'Hello world form log monitor';
+LogMonitoring::sendByMail($message);
--- ip_track.py
+++ ip_track.py
@@ -2,22 +2,19 @@
  * @author Shadoworker5 Dev
  * @email shadoworker5.dev@gmail.com
  * @create date 2022-09-14 23:37:22
- * @modify date 2022-10-06 09:15:51
+ * @modify date 2023-07-04 11:22:39
  * @desc [description]
 """
 import argparse
 from socket import gethostbyaddr
 import requests
 from os import system, name as sys_name
-from art import tprint
 
-API_TOKEN = { "IPStack": "YOUR_TOKEN" }
-R = '\033[31m'  # red
-G = '\033[32m'  # green
-C = '\033[36m'  # cyan
-W = '\033[0m'   # white
-Y = '\033[33m'  # yellow
-VERSION = '1.0.0'
+API_TOKEN   = {"IPStack": "YOUR_TOKEN"}
+RED         = '\033[31m'
+GREEN       = '\033[32m'
+WHITE       = '\033[0m' 
+VERSION     = '1.0.1'
 
 def localiseIP(ip):
     api_ipstack = f"http://api.ipstack.com/{ip}?access_key={API_TOKEN['IPStack']}"
@@ -25,45 +22,41 @@ def localiseIP(ip):
     try:
         response = requests.get(api_ipstack)
         if response.status_code == 200:
-            response = response.json()
-            fai = requests.get(f'{api_ipwhois}').json()
-            map_url = url = f"https://maps.google.com/?q={response['latitude']},{response['longitude']}"
+            response    = response.json()
+            fai         = requests.get(f'{api_ipwhois}').json()
+            map_url     = f"https://maps.google.com/?q={response['latitude']},{response['longitude']}"
             print(f"Target public IP: {response['ip']}")
             print(f"Type address    : {response['type']}")
             print(f"Continent code  : {response['continent_code']}")
             print(f"Continent name  : {response['continent_name']}")
             print(f"Country code    : {response['country_code']}")
-            print(f"Country name    : {R}{response['country_name']}{G}")
-            print(f"Region code     : {R}{response['region_code']}{G}")
-            print(f"Region name     : {R}{response['region_name']}{G}")
-            print(f"City            : {R}{response['city']}{G}")
+            print(f"Country name    : {GREEN}{response['country_name']}{GREEN}")
+            print(f"Region code     : {GREEN}{response['region_code']}{GREEN}")
+            print(f"Region name     : {GREEN}{response['region_name']}{GREEN}")
+            print(f"City            : {GREEN}{response['city']}{GREEN}")
             print(f"FAI             : {fai['org']}")
-            print(f"Latitude        : {R}{response['latitude']}{G}")
-            print(f"Longitude       : {R}{response['longitude']}{G}")
-            print(f"Maps Url        : {R}{map_url}{G}")
+            print(f"Map Url         : {GREEN}{map_url}{GREEN}")
             print(f"Capital         : {response['location']['capital']}")
             print(f"Code languages  : {response['location']['languages'][0]['code']}")
             print(f"Name languages  : {response['location']['languages'][0]['name']}")
             print(f"Native languages: {response['location']['languages'][0]['native']}")
             print(f"Calling code    : {response['location']['calling_code']}")
-            print(f"Country flag    : {response['location']['country_flag']}{W}")
+            print(f"Country flag    : {response['location']['country_flag']}")
     except Exception as e:
         result = requests.get(api_ipwhois)
         if result.status_code == 200:
-            result = result.json()
-            map_url = url = f"https://maps.google.com/?q={result['latitude']},{result['longitude']}"
+            result  = result.json()
+            map_url = f"https://maps.google.com/?q={result['latitude']},{result['longitude']}"
             print(f"Target public IP: {result['ip']}")
             print(f"Type address    : {result['type']}")
             print(f"Continent code  : {result['continent_code']}")
             print(f"Continent name  : {result['continent']}")
             print(f"Country code    : {result['country_code']}")
-            print(f"Country name    : {R}{result['country']}{G}")
-            print(f"Region name     : {R}{result['region']}{G}")
-            print(f"City            : {R}{result['city']}{G}")
+            print(f"Country name    : {GREEN}{result['country']}{GREEN}")
+            print(f"Region name     : {GREEN}{result['region']}{GREEN}")
+            print(f"City            : {GREEN}{result['city']}{GREEN}")
             print(f"FAI             : {result['org']}")
-            print(f"Latitude        : {R}{result['latitude']}{G}")
-            print(f"Longitude       : {R}{result['longitude']}{G}")
-            print(f"Maps Url        : {R}{map_url}{G}")
+            print(f"Maps Url        : {GREEN}{map_url}{GREEN}")
             print(f"Capital         : {result['country_capital']}")
             print(f"Borders         : {result['country_neighbours']}")
             print(f"Calling code    : {result['country_phone']}")
@@ -71,13 +64,13 @@ def localiseIP(ip):
             print(f"Local currency  : {result['currency']}")
             print(f"Code currency   : {result['currency_code']}")
             print(f"Currency rates  : {result['currency_rates']}")
-            print(f"Currency plural : {result['currency_plural']}{W}")
+            print(f"Currency plural : {result['currency_plural']}")
 
 def getHostIP():
     ip = ''
     try:
-        response = requests.get('https://api64.ipify.org?format=json').json()
-        ip = response["ip"]
+        response    = requests.get('https://api64.ipify.org?format=json').json()
+        ip          = response["ip"]
     except Exception as e:
         ip = '127.0.0.1'
     return ip
@@ -106,33 +99,35 @@ def cleanScreen():
     
 if __name__ == '__main__':
     try:
+        banner = '''
+         ___  ____   _____  ____      _      ____  _  __
+        |_ _||  _ \ |_   _||  _ \    / \    / ___|| |/ /
+         | | | |_) |  | |  | |_) |  / _ \  | |    | ' / 
+         | | |  __/   | |  |  _ <  / ___ \ | |___ | . \ 
+        |___||_|      |_|  |_| \_\/_/   \_\ \____||_|\_\\
+        '''
         parser = argparse.ArgumentParser()
         parser.add_argument('-o', '--own', help='Geolocalize your own IP', action='store_true')
         parser.add_argument('-t', '--target', help='Geolocalize target IP', action='store_true')
         args = parser.parse_args()
         
         cleanScreen()
-        print(f'{R}')
-        tprint('IPTRACK')
-        print(f'{G}')
+        print(RED)
+        print(banner)
+        print(GREEN)
         
         if args.target:
             target_ip = getTargetIP()
             print(f'Your Target IP  : {target_ip}')
             print(f'Your Target Name: {getHostName(target_ip)}')
             localiseIP(target_ip)
-            
-        if args.own:
+        elif args.own:
             host_local_ip = getHostIP()
             print(f'Your IP         : {host_local_ip}')
             print(f'Your host Name  : {getHostName(host_local_ip)}')
             localiseIP(host_local_ip)
-            
-        if args.target is False and args.own is False:
+        else:
             parser.print_help()
-    except KeyboardInterrupt as e:
-        print('KeyboardInterpt .....{W}')
-        exit(1)
     except Exception as e:
-        print(f'Errorr: {e}{W}')
+        print(f'Errorr: {e}{WHITE}')
         exit(1)
