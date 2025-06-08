967b206cbe4021be217fcbfe74c2e7615788054e
Kassoum Traore <kassoum.traore@africasys.com>
2025-06-08 09:22:57 +0000

    make file bloqued in linux

---
+++ block_file.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# modification ou suppression, utilisez la commande suivante :
+sudo chattr +i /home/flo/key.txt
+
+# commande lsattr sert à lister les attributs étendus d'un fichier.
+lsattr /home/flo/key.txt
